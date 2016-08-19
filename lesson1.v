b37aec5cf449f106c4078986870122d65a69d9d3
Enrico Tassi <gares@fettunta.org>
2016-08-19 13:49:56 +0200

    wip

--- lesson1.v
+++ lesson1.v
@@ -75,10 +75,48 @@ to say:
  - andb negb orb
  - case
 
-boolean connective, computation is progress, proof by cases
-Objective: infitude of primes
+boolean connective, computation is progress, proof by cases *)
 
-proof sketch, we need arithmetic (and a library)!
+Module BoolLogic.
+
+Definition negb b := if b then false else true.
+
+Local Notation "~~ b" := (negb b).
+
+Lemma negbK b : ~~ ~~ b = b.
+Proof.
+(*D*) case: b.
+(*D*) by []. Qed.
+
+Definition andb b1 b2 :=
+(*D*) if b1 then b2 else false.
+
+Local Notation "a && b" := (andb a b).
+
+Lemma andbC b1 b2 : b1 && b2 = b2 && b1.
+Proof.
+(*D*) by case: b1; case: b2. Qed.
+
+Definition orb b1 b2 :=
+(*D*) if b1 then true else b2.
+
+Local Notation "a || b" := (orb a b).
+
+Lemma negb_and b1 b2 : ~~ (b1 && b2) = ~~ b1 || ~~ b2.
+Proof.
+(*D*) by case: b1; case: b2. Qed.
+
+End BoolLogic.
+
+(** ** Objective: infitude of primes
+
+proof sketch
+
+-------------------------------------------- *)
+
+(** ** Requirements:
+
+we need arithmetic (and a library)!
 A bit of arithmetic, ssr style
 
 to say:
@@ -89,21 +127,26 @@ to say:
 
 -------------------------------------------- *)
 
+About dvdn_fact.
+About contraL.
+
 Lemma example m p : prime p ->
   p %| m `! + 1 -> ~~ (p <= m).
 Proof.
-move=> prime_p.
-apply: contraL.
-move=> leq_p_m.
-rewrite dvdn_addr.
-  rewrite gtnNdvd.
-    by [].
-    by [].
-  by apply: prime_gt1.
-apply: dvdn_fact.
-rewrite leq_p_m andbT.
-by apply: prime_gt0.
-Qed.
+(*D*) move=> prime_p.
+(*D*) apply: contraL.
+(*D*) move=> leq_p_m.
+(*D*) rewrite dvdn_addr.
+(*D*)   rewrite gtnNdvd.
+(*D*)     by [].
+(*D*)     by [].
+(*D*)   by apply: prime_gt1.
+(*D*) apply: dvdn_fact.
+(*D*) rewrite leq_p_m andbT.
+(*D*) by apply: prime_gt0.
+(*D*) Qed.
+
+(** -------------------------------------------- *)
 
 (** ** Bool is not enough
 
@@ -113,8 +156,23 @@ to say:
   - exists/curry howard
   - bool/Prop
   - reflect
+*)
 
--------------------------------------------- *)
+Module PropLogic.
+
+Inductive and (A B:Prop) : Prop :=
+  conj : A -> B -> A /\ B.
+
+Local Notation "A /\ B" := (and A B).
+
+Inductive ex (A:Type) (P:A -> Prop) : Prop :=
+  ex_intro : forall x:A, P x -> ex (A:=A) P.
+
+Local Notation "'exists' x , p" := (ex (fun x => p)).
+
+End PropLogic.
+
+(** -------------------------------------------- *)
 
 (** ** Views and intro patterns
 
@@ -129,13 +187,15 @@ to say:
 
 Lemma dvdn_fact m n : 0 < m <= n -> m %| n`!.
 Proof.
-case: m => //= m; elim: n => //= n IHn; rewrite ltnS leq_eqVlt.
-by case/orP=> [/eqP-> | /IHn]; [apply: dvdn_mulr | apply: dvdn_mull].
-Qed.
+(*D*) case: m => //= m; elim: n => //= n IHn; rewrite ltnS leq_eqVlt.
+(*D*) by case/orP=> [/eqP-> | /IHn]; [apply: dvdn_mulr | apply: dvdn_mull].
+(*D*) Qed.
 
 Lemma prime_above m : exists2 p, m < p & prime p.
 Proof.
-have /pdivP[p pr_p p_dv_m1]: 1 < m`! + 1 by rewrite addn1 ltnS fact_gt0.
-exists p => //; rewrite ltnNge; apply: contraL p_dv_m1 => p_le_m.
-by rewrite dvdn_addr ?dvdn_fact ?prime_gt0 // gtnNdvd ?prime_gt1.
-Qed.
+(*D*) have /pdivP[p pr_p p_dv_m1]: 1 < m`! + 1 by rewrite addn1 ltnS fact_gt0.
+(*D*) exists p => //; rewrite ltnNge; apply: contraL p_dv_m1 => p_le_m.
+(*D*) by rewrite dvdn_addr ?dvdn_fact ?prime_gt0 // gtnNdvd ?prime_gt1.
+(*D*) Qed.
+
+
